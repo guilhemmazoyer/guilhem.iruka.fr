1c8e74c3470bfcc326f53cbd1ed1ba82b8a8baaf
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-07-12 23:33:39 +0200

    Intégration automatique du menu de navigation depuis un fichier "topmenu.js"

---
+++ jslib/topmenu.js
@@ -0,0 +1,13 @@
+document.write(`
+<nav id="topmenu">
+    <ul id="topmenu-list">
+        <li class="left"><a class="topmenu-items" href="./index.html">Accueil</a></li>
+        <li class="left"><a class="topmenu-items" href="./programmation.html">Programmation</a></li>
+        <li class="left"><a class="topmenu-items" href="./gamedesign.html">Game Design</a></li>
+        <li class="left"><a class="topmenu-items" href="./musiques.html">Musiques</a></li>
+        <li class="left"><a class="topmenu-items" href="./wt/index.html">Wyrd's Tales</a></li>
+        <li class="right"><a class="topmenu-items" href="https://www.linkedin.com/in/guilhem-mazoyer-570637214/" target="_blank"><img src="assets/images/icons/linkedin_icon.jpg" alt=""></a></li>
+        <li class="right"><a class="topmenu-items" href="https://github.com/guilhemmazoyer" target="_blank"><img src="assets/images/icons/github.png" alt=""></a></li>
+    </ul>
+</nav>
+`);
