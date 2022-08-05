46f553765cd988bf4513679df38396a03cb55d6b
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-08-05 11:16:44 +0200

    correction suite à un renommage

--- jslib/topmenu.js
+++ jslib/topmenu.js
@@ -7,7 +7,7 @@ document.write(`
         <li class="left"><a class="topmenu-items" href="./musiques.html">Musiques</a></li>
         <li class="left"><a class="topmenu-items" href="./wt/index.html">Wyrd's Tales</a></li>
         <li class="right"><a class="topmenu-items" href="https://www.linkedin.com/in/guilhem-mazoyer-570637214/" target="_blank"><img src="assets/images/icons/linkedin_icon.jpg" alt=""></a></li>
-        <li class="right"><a class="topmenu-items" href="https://github.com/guilhemmazoyer" target="_blank"><img src="assets/images/icons/github.png" alt=""></a></li>
+        <li class="right"><a class="topmenu-items" href="https://github.com/guilhemmazoyer" target="_blank"><img src="assets/images/icons/github_icon.png" alt=""></a></li>
     </ul>
 </nav>
 `);
