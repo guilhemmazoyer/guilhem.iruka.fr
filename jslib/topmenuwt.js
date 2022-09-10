bae882a14cad818f39da421b7c7631ffd311d646
guilhemmazoyer <guilhem.mazoyer@laposte.net>
2022-09-10 23:31:10 +0200

    Changement du code pour utiliser les nouvelles images et ajouter ArtStation dans le menu

--- jslib/topmenuwt.js
+++ jslib/topmenuwt.js
@@ -6,8 +6,9 @@ document.write(`
         <li class="left"><a class="topmenu-items" href="../personnels.html">Projets Personnels</a></li>
         <li class="left"><a class="topmenu-items" href="../musiques.html">Musiques</a></li>
         <li class="left"><a class="topmenu-items" href="./index.html">Wyrd's Tales</a></li>
-        <li class="right"><a class="topmenu-items" href="https://www.linkedin.com/in/guilhem-mazoyer-570637214/" target="_blank"><img src="../assets/images/icons/linkedin_icon.jpg" alt=""></a></li>
-        <li class="right"><a class="topmenu-items" href="https://github.com/guilhemmazoyer" target="_blank"><img src="../assets/images/icons/github_icon.png" alt=""></a></li>
+        <li class="right"><a class="topmenu-items" href="https://www.linkedin.com/in/guilhem-mazoyer-570637214/" target="_blank"><img src="../assets/images/icons/linkedin_icon.png" alt="LinkedIn"></a></li>
+        <li class="right"><a class="topmenu-items" href="https://github.com/guilhemmazoyer" target="_blank"><img src="../assets/images/icons/github_icon.png" alt="GitHub"></a></li>
+        <li class="right"><a class="topmenu-items" href="https://www.artstation.com/gwigwi" target="_blank"><img src="../assets/images/icons/artstation_icon.png" alt="ArtStation"></a></li>
     </ul>
 </nav>
 `);
