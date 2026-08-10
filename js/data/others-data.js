3629e89ed245aa5fd8ae2a255c5d99b82077853b
Guilhem <guilhem.mazoyer@laposte.net>
2026-08-10 10:40:16 +0200

    Add my Ship of Fools mod to the 'others' projects Add an icon for C# projects Reduce the size of the image of my portfolio project from 1080p to 720p Change some 'Others' texts that were still in french

--- js/data/others-data.js
+++ js/data/others-data.js
@@ -4,6 +4,20 @@
 
 const others = [
     {
+        title: "Mod - Ship of Fools",
+        thumbnail: "assets/images/SoF_Mod.jpg",
+        link: "https://www.nexusmods.com/shipoffools/mods/6",
+        year: "2026",
+        tools: ["csharp"]
+    },
+    {
+        title: "Portfolio website",
+        thumbnail: "assets/images/portfolio.jpg",
+        link: "https://guilhem.iruka.fr/",
+        year: "2026",
+        tools: ["html", "css", "js", "cursor"]
+    },
+    {
         title: "3D Cube Renderer",
         thumbnail: "assets/images/Py3DShapeRenderer.jpg",
         link: "https://github.com/guilhemmazoyer/Py3DShapeRenderer",
@@ -11,21 +25,14 @@ const others = [
         tools: ["python"]
     },
     {
-        title: "Modèles 3D",
+        title: "3D Models",
         thumbnail: "assets/images/blender.jpg",
         link: "https://www.artstation.com/guilhemmazoyer",
         year: "2022",
         tools: ["blender"]
     },
     {
-        title: "Site portfolio",
-        thumbnail: "assets/images/portfolio.jpg",
-        link: "https://guilhem.iruka.fr/",
-        year: "2026",
-        tools: ["html", "css", "js", "cursor"]
-    },
-    {
-        title: "Parseur d'articles scientifiques",
+        title: "Scientific Article Parser",
         thumbnail: "assets/images/parseur.jpg",
         link: "https://github.com/guilhemmazoyer/Parseur_lbgl",
         year: "2022",
